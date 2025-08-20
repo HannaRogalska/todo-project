5cf1ca3dced6bbbd1a4a997d3c8fcde289b6536f
Hanna Rogalska <hanrogalska@gmail.com>
2025-08-20 16:02:20 +0200

    change

--- src/components/TodoList/TodoList.test.tsx
+++ src/components/TodoList/TodoList.test.tsx
@@ -1,4 +1,4 @@
-import { getAllByRole, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, it, expect, vi } from "vitest";
 import userEvent from "@testing-library/user-event";
 import TodoList from "./TodoList";
